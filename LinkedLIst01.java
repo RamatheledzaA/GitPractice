8154cae142c2ac80dc657aafb10401c226e73279
RamatheledzaA <ramatheledzaa@gmail.com>
2026-03-01 14:13:23 +0200

    created a class node

--- LinkedLIst01.java
+++ LinkedLIst01.java
@@ -3,5 +3,13 @@
 
 
 public class LinkedLIst01 {
-    
+     //create a node classs
+    static class Node{
+        int data ;
+        Node next;
+        Node ( int data){
+            this.data = data;
+            this.next = null;
+        }
+     }
 }
