d43aee0f23f5a7847aef00d8d50f0023ecce3694
RamatheledzaA <ramatheledzaa@gmail.com>
2026-03-01 14:48:13 +0200

    update my code

--- LinkedList02.java
+++ LinkedList02.java
@@ -15,5 +15,31 @@ public class LinkedList02 {
             this.prev = null ;
         }
     }
-    
+    public static void main(String[] args){
+        // create  new nodes
+        Node node1 = new Node(5);
+        Node node2 = new Node(12);
+        Node node3 = new Node(2);
+        Node node4 = new Node(25);
+        Node node5 = new Node(27);
+
+        // now let's link the nodes
+        node1.next = node2;
+        node1.prev = node5;
+
+        node2.next = node3;
+        node2.prev = node1;
+
+        node3.next = node4;
+        node3.prev = node2;
+
+        node4.next =node5;
+        node4.prev = node3;
+
+        node5.next = node1;
+        node5.prev = node4;
+
+
+
+    }
 }
