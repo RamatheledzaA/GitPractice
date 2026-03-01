1acf65772658aae6f7e27edc4fc5ea68c3d998c9
RamatheledzaA <ramatheledzaa@gmail.com>
2026-03-01 14:26:34 +0200

    update my code

--- LinkedLIst01.java
+++ LinkedLIst01.java
@@ -12,4 +12,30 @@ public class LinkedLIst01 {
             this.next = null;
         }
      }
+
+     public static void main(String[] args){
+         // create 4 new node
+         Node node1 = new Node(3);
+         Node node2 = new Node(5);
+         Node node3 = new Node(15);
+         Node node4 = new Node(5);
+
+         // now start linking the nodes
+         node1.next = node2;
+         node2.next = node3;
+         node3.next = node4;
+         node4.next = node1; // this indicates a circular link
+
+         Node currentNode = node1;
+         Node startNode = node1 ;
+         System.out.print( currentNode.data + " - >");
+         currentNode = currentNode.next;
+
+         while ( currentNode != startNode){
+             System.out.print( currentNode.data);
+             currentNode = currentNode.next;
+         }
+         System.out.println("------");
+
+     }
 }
