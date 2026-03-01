8e5c3e6c858419202cfe1665e71e1d7c135b3348
RamatheledzaA <ramatheledzaa@gmail.com>
2026-03-01 14:37:06 +0200

    update my code

--- LinkedList02.java
+++ LinkedList02.java
@@ -2,4 +2,18 @@
 // prep: Circular singly linked list
 
 public class LinkedList02 {
+
+    // start by creating a class node
+    static class Node{
+        int data;
+        Node next;
+        Node prev;
+
+        Node ( int data){
+            this.data = data;
+            this.next = null;
+            this.prev = null ;
+        }
+    }
+    
 }
