5a5acd31ce5add2df9366249c2cace8bc40893af
RamatheledzaA <ramatheledzaa@gmail.com>
2026-03-08 14:11:36 +0200

    update code

--- AnagramHash.java
+++ AnagramHash.java
@@ -1,9 +1,34 @@
 // ramatheledza adzisani 4484298
 // Write a Java program that reads words and groups anagrams.
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class AnagramHash {
-    public static void main(String [] args){
+    public static void main(String [] args) throws Exception{
+
+        // read a file
+        File file =  new File ("book.txt") ;
+        Scanner scan = new Scanner (file);
+        HashMap < String,Integer> wordCount = new HashMap<>();
+
+        while (scan.hasNext()){
+            String word = scan.next().toLowerCase();
+            word = word.replaceAll("[a - z]", " ");
+            if (wordCount.containsKey(word)){
+                wordCount.put(word , wordCount.get(word + 1));
+            }else{
+                wordCount.put(word , 1);
+            }
+        }
+        scan.close();
+        for (String word : wordCount.keySet()){
+            System.out.println(word + ":" + wordCount.get(word));
+        }
+
         
+
     }
 
 }
