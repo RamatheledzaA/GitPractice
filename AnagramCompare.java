bf7aaaa52797c3d49b6581a81e851c7e097c1a35
RamatheledzaA <ramatheledzaa@gmail.com>
2026-03-09 00:56:59 +0200

    Update code

--- AnagramCompare.java
+++ AnagramCompare.java
@@ -14,41 +14,42 @@ public class AnagramCompare {
     // ------Sorting method anagram------
     public static void SortingMethod() throws Exception{
 
-        HashMap <String,ArrayList<String>> map = new HashMap<>();
-        File file  = new File("\"C:/Users/user/IdeaProjects/My_COS211 and 212 projects/src/book.txt/");
+        HashMap<String, ArrayList<String>> map = new HashMap<>();
+        File file = new File("C:/Users/user/IdeaProjects/My_COS211 and 212 projects/src/book.txt/");
         Scanner sc = new Scanner(file);
 
-        while(sc.hasNext()){
-
+        while (sc.hasNext()) {
             String word = sc.next().toLowerCase();// read the next word
             word = word.replaceAll("[^a-z]", ""); // remove punctuations
 
-            if(word.length() == 0){
+            if (word.length() == 0) {
                 continue;
             }
 
             // now create a character array and sort it
-            char [] c = word.toCharArray();
+            char[] c = word.toCharArray();
             Arrays.sort(c);
 
             // now create a key
-            String key = new String (word);
+            String key = new String(word);
 
             // if key is not in the map
-            if(!map.containsKey(key)){
-                map.putIfAbsent(key , new ArrayList<>());
+            if (!map.containsKey(key)) {
+                map.putIfAbsent(key, new ArrayList<>());
             }
             map.get(key).add(word);
 
         }
         sc.close();
+        
+        PrintLargestGroup(map);
     }
 
     // -------Frequency method------
     public static void FrequencyMethod() throws Exception{
-
         HashMap <String,ArrayList<String>> map = new HashMap<>();
-        Scanner sc = new Scanner(new File("C:/Users/user/IdeaProjects/My_COS211 and 212 projects/src/book.txt/"));
+        File file = new File("C:/Users/user/IdeaProjects/My_COS211 and 212 projects/src/book.txt/");
+        Scanner sc = new Scanner(file);
 
         while (sc.hasNext()){
             String word = sc.next().toLowerCase();
@@ -72,10 +73,53 @@ public class AnagramCompare {
             map.get(key).add(word);
         }
         sc.close();
+        PrintLargestGroup(map);
     }
-    public static void main(String[] args)  {
+    // print the largest group in the anagram and print the smallest group in the anagram
+    public static void PrintLargestGroup(HashMap <String,ArrayList<String>> map){
+
+        ArrayList<String> LargestGroup = new ArrayList<>();
+        ArrayList<String> SmallestGroup = new ArrayList<>();
+        for(ArrayList<String> group : map.values()){
+            if( group.size() > LargestGroup.size()){
+                LargestGroup = group;
+            }else {
+                SmallestGroup = group;
+            }
+        }
+        System.out.println("-------------The Smallest group-------");
+        System.out.println(SmallestGroup);
+        System.out.println("Size: " + SmallestGroup.size());
+
+        System.out.println("-------------The Largest group-------");
+        System.out.println(LargestGroup);
+        System.out.println("Size: " + LargestGroup.size());
+
+    }
+
+    // ----- time complexity of all the algorithms
+    public static void main(String[] args) throws Exception {
+
+        // time complexity for sorting method
+        System.out.println("------Sorting Method Time Complexity------");
+        long startSort = System.nanoTime();
+        SortingMethod();
+        long endSort = System.nanoTime();
+
+        System.out.println(" Sorting method time : " + (endSort - startSort) + "ns");
+
+        System.out.println("-------------------------------");
+
+        // time complexity for frequency method
+        System.out.println("------Frequency Method Time Complexity------");
+        long startfreq = System.nanoTime();
+        FrequencyMethod();
+        long endfreq = System.nanoTime();
+
+        System.out.println(" Frequency method time : " + (endfreq - startfreq) + "ns");
 
 
 
     }
+
 }
