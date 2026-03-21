012beddf8cdaa695cb75c763af9928c23f24d962
RamatheledzaA <ramatheledzaa@gmail.com>
2026-03-21 22:59:58 +0200

    update my code

--- Level_Order_Traversal.java
+++ Level_Order_Traversal.java
@@ -1,2 +1,37 @@
+// Ramatheledza Adzisani 4484298
+
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.Queue;
+
 public class Level_Order_Traversal {
+
+    // let' create a node class
+    public static class Node{
+        char data;
+        Node left,right;
+        public Node ( char data){
+            this.data = data;
+            left= right= null;
+
+        }
+    }
+    public static void levelOrder(Node root){
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+
+        while(!q.isEmpty()) {
+            Node current = q.poll();
+            System.out.print(current.data + " ");
+
+            if (current.left != null){
+                q.add(current.left);
+            }
+            if (current.right != null){
+                q.add(current.right);
+            }
+
+        }
+    }
+
 }
