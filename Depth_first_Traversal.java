218906864b266ab5ab84ed86d445d8719f59e94d
RamatheledzaA <ramatheledzaa@gmail.com>
2026-03-21 23:06:57 +0200

    let's work on the pre order traversal

--- Depth_first_Traversal.java
+++ Depth_first_Traversal.java
@@ -1,5 +1,19 @@
+import javax.swing.tree.TreeNode;
+
 // Ramatheledza Adzisai 4484298
 public class Depth_first_Traversal {
 
-    
+    // let's create a treeNode class
+    public static class TreeNode{
+        String data;
+        TreeNode left;
+        TreeNode right;
+        public TreeNode (String data){
+            this.data= data;
+            this.left = null;
+            this.right =  null;
+
+        }
+    }
+
 }
